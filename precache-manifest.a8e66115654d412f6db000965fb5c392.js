d649cbbf12504a70e399f421dcceb60976ff6567
llewyn <kbj2060@naver.com>
2021-02-02 15:44:55 +0900

    Updates

---
+++ precache-manifest.a8e66115654d412f6db000965fb5c392.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951b06f098daed2586e545e9291115a2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eb56044cf22907742d76",
+    "url": "/portfolio/static/css/main.3d34be92.chunk.css"
+  },
+  {
+    "revision": "019940cb3e5c947f158d",
+    "url": "/portfolio/static/js/2.7c24f566.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/portfolio/static/js/2.7c24f566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb56044cf22907742d76",
+    "url": "/portfolio/static/js/main.f4de0a7f.chunk.js"
+  },
+  {
+    "revision": "26351f6e611646d04f58",
+    "url": "/portfolio/static/js/runtime-main.94e8fcfa.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/portfolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/portfolio/static/media/your photo.b9d7d448.png"
+  }
+]);
