b0910f3a34ebf7c5baf24e5742437922dde07fc9
llewyn <kbj2060@naver.com>
2021-08-19 00:36:25 +0900

    [New Feature] Slider card added

--- src/components/IntroduceBusiness/IntroduceBusiness.tsx
+++ src/components/IntroduceBusiness/IntroduceBusiness.tsx
@@ -1,24 +1,53 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import './IntroduceBusiness.css'
-import "swiper/swiper.min.css";
-import "swiper/components/pagination/pagination.min.css"
 import { Typography } from "@material-ui/core";
 import SwiperCore, {Navigation} from 'swiper/core';
+import './IntroduceBusiness.css'
+import './IntroduceBusinessCard.css'
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
 
 // install Swiper modules
 SwiperCore.use([Navigation]);
 
+interface CardInfo {
+    address : string,
+    status : string,
+    price : string,
+    date : string,
+    picture : string
+}
+
+function IntroduceBusinessCard(props: CardInfo) {
+    return (
+        <div className="introduce_business_card_root">
+            <img alt="intro_image" src={process.env.PUBLIC_URL + props.picture} />
+        </div>
+    );
+}
+
+
 export default function IntroduceBusiness() {
+    const construction_info: CardInfo = require('../../constructions.json')
+    console.log(construction_info)
+
+    function render_all_cards () {
+        return Object.values(construction_info).map((info) => {
+            return (
+                <SwiperSlide>
+                    <IntroduceBusinessCard key={info.address} {...info} />
+                </SwiperSlide>
+            )
+        })
+    }
+
     return (
         <div className="introduce_business_root">
-            <Typography className="title">사업소개</Typography>
+            <div className="title-div">
+                <Typography className="title">PROJECTS</Typography>
+            </div>
             <Swiper navigation={true} className="business-swiper">
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
+                {render_all_cards()}
             </Swiper>
         </div>
     );
